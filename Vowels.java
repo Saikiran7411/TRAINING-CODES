bdb60fa25c0a98304f1804761b5c16ef1239978c
Saikiran Gadge <105089217+Saikiran7411@users.noreply.github.com>
2022-09-27 23:41:39 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,40 @@
+class Vowels
+{
+	public static void vowels(char alphabets)
+	{
+		switch(alphabets)
+		{
+		case'A':System.out.println("A is vowel");
+			break;
+		case'a':System.out.println("a is vowel");
+			break;
+		case'E':System.out.println("E is vowel");
+			break;
+		case'e':System.out.println("e is vowel");
+			break;
+		case'I':System.out.println("I is vowel");
+			break;
+		case'i':System.out.println("i is vowel");
+			break;
+		case'O':System.out.println("O is vowel");
+			break;
+		case'o':System.out.println("o is vowel");
+			break;
+		case'U':System.out.println("U is vowel");
+			break;
+		case'u':System.out.println("u is vowel");
+			break;
+		default:System.out.println("not a vowel");
+
+		}
+	}
+	public static void main(String[] args)
+	 {
+	 	vowels('A');
+	 	vowels('a');
+	 	vowels('s');
+	 	vowels('i');
+	 	vowels('o');
+		
+	}
+}
